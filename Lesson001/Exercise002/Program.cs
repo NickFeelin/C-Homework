4c72c75b2d667fb1a958a9f25631bb26fcd19198
Nick Filin <flyer.fil@list.ru>
2022-08-21 10:41:00 +0700

    add Exercise 002

---
+++ Lesson001/Exercise002/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+
+if (numberA > numberB)
+    Console.WriteLine($"Число {numberA} больше числа {numberB} ");
+else
+    Console.WriteLine($"Число {numberB} больше числа {numberA} ");
+
+
