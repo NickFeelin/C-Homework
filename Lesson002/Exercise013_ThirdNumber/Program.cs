83a9a9737a6df0f86e390161df7c3c9fb8f7d8c3
Nick Filin <flyer.fil@list.ru>
2022-08-21 10:54:32 +0700

    add Exercise 013 Third Number

---
+++ Lesson002/Exercise013_ThirdNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿/*  Напишите программу, которая выводит третью цифру 
+    заданного числа или сообщает, что третьей цифры нет.
+*/
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+
+int one = (number / 100) % 10;
+//int hundred = (number - one) / 100;
+//int third = hundred % 10;
+
+if (one > 0)
+    Console.Write($"Третья цифра числа {number} это {one}");
+else
+    Console.Write("Третьей цифры нет");
