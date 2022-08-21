42f183ed11ee56933ce06b44b630c215a88cc775
Nick Filin <flyer.fil@list.ru>
2022-08-21 11:02:44 +0700

    add Exercise 023 Table Cube

---
+++ Lesson003/Exercise023_TableCube/Program.cs
@@ -0,0 +1,9 @@
+﻿/*Напишите программу, которая принимает на вход число (N)
+и выдает таблицу кубов чисел от 1 до N
+*/
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= N; i++)
+    Console.Write(Math.Pow(i, 3) + ", ");
