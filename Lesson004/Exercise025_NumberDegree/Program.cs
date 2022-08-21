e15fd15783791dbf20b9524ec654da4689a04d21
Nick Filin <flyer.fil@list.ru>
2022-08-21 11:09:05 +0700

    add Exercise 025 Number Degree

---
+++ Lesson004/Exercise025_NumberDegree/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Напишите цикл, который принимает на вход два числа (А и В) и возводит число А в натуральную степень В
+*/
+
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите степень: ");
+int b = int.Parse(Console.ReadLine());
+
+int num = 1;
+
+int Degree()
+{
+    for(int i = 0; i < b; i++)
+    {
+        num *= a;
+    }
+    return num;
+}
+Degree();
+
+Console.Write($"Число {a} в степени {b} равно {num}");
+Console.WriteLine();
