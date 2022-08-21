47ef99e6578d59c87113023eab77ce539127b3a6
Nick Filin <flyer.fil@list.ru>
2022-08-21 11:11:00 +0700

    add Exercise 027 Sum Numbers

---
+++ Lesson004/Exercise027_SumNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Напишите функцию и запустите ее, которая принимает на вход число и выдает сумму цифр в числе
+*/
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int sum = 0;
+
+int Numbers()
+{
+    int N = num;
+
+    while (N > 0)
+    {
+        sum = sum + N % 10;
+        N = N / 10;
+    }
+    return sum;
+}
+Numbers();
+
+Console.Write($"Сумма цифр числа {num} равна {sum}");
+Console.WriteLine();
