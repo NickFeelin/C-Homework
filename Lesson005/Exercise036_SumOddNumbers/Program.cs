463cad9dc614e55f8c04308906dcc3f37123f85b
Nick Filin <flyer.fil@list.ru>
2022-08-21 18:21:19 +0700

    add Exercise 036 Sum Odd Numbers

---
+++ Lesson005/Exercise036_SumOddNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов,
+стоящих на нечетных позициях
+*/
+
+int[] GetArray(int size)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(-99, 100);
+    }
+    return res;
+}
+
+int OddSum(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 == 1) sum += arr[i]; 
+    }
+    return sum;
+}
+
+int[] array = GetArray(10);
+Console.WriteLine(String.Join(" ", array));
+Console.WriteLine();
+Console.WriteLine($"{OddSum(array)}");
