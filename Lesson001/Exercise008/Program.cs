d4d0e93571b9b0765b0cf0588556ba5af7d887db
Nick Filin <flyer.fil@list.ru>
2022-08-21 10:49:26 +0700

    add Exercise 008

---
+++ Lesson001/Exercise008/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.Write("Четные числа: ");
+for (int index = 0; index <= number; index++)
+{
+    if (index % 2 == 0)
+        Console.Write("{0} ", index);
+}
