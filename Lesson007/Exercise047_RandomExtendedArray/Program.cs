599f5ed9e3750697a0efbce018647d40a51b7b72
Nick Filin <flyer.fil@list.ru>
2022-09-02 17:01:59 +0700

    add Exercise 047 Random Extended Array

---
+++ Lesson007/Exercise047_RandomExtendedArray/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Задайте двумерный массив размером m * n, заполненный случайными вещественными числами
+*/
+
+void FillArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {   
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = Math.Round((new Random().Next(-9, 10) * new Random().NextDouble()), 1);
+        }
+    Console.WriteLine();
+    }
+}
+
+void PrintArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {   
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите количество столбцов: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите количество строк: ");
+int n = int.Parse(Console.ReadLine());
+
+double[,] matrix = new double[m, n];
+FillArray(matrix);
+PrintArray(matrix);
