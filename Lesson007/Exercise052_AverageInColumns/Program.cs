64e9d4c7b18c61c9d087f1d556654bd14bea1590
Nick Filin <flyer.fil@list.ru>
2022-09-02 23:54:56 +0700

    add Exercise 052 Average In Column

---
+++ Lesson007/Exercise052_AverageInColumns/Program.cs
@@ -0,0 +1,58 @@
+﻿/*
+Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце
+*/
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+        Console.WriteLine();
+    }
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindAverageColumn(int[,] matr)
+{   
+    int[] sum = new int[matr.GetLength(1)];
+    int count = 0;
+    int sumColum = 0;
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        sum[i] = 0;
+            for (int k = 0; k < matr.GetLength(0); k++)
+            {
+                for (int j = 0; j < matr.GetLength(1); j++)
+                {
+                    sum[i] += matr[k, j];
+                    sumColum = sum[i];
+                    count++;
+                }
+                Console.WriteLine();
+            }
+    {
+        Console.WriteLine(sumColum / count);
+    }
+    }
+}
+
+int[,] matrix = new int[4, 5];
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+FindAverageColumn(matrix);
