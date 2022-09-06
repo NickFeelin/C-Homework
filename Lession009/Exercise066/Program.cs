731acd1c13e16e9702a49dfe46279dda9383c3f5
Nick Filin <flyer.fil@list.ru>
2022-09-07 01:38:18 +0700

    add Exercise 066 Sum Recursion

---
+++ Lession009/Exercise066/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Задайте значения M N. Напишите программу, которая найдет сумму 
+натуральных элементов в промежутке от M до N
+*/
+
+int SumRec(int M, int N)
+{
+   return M == N ? N : M + SumRec(M + 1, N);
+}
+
+Console.Write("M = ");
+int min = int.Parse(Console.ReadLine());
+Console.Write("N = ");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine(SumRec(min, number));
